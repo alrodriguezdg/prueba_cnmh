d6158def763ea977c0b9aeb4914ec2849c65a7d3
Al Rodriguez <alrodriguezdg@gmail.com>
2021-02-17 16:04:45 -0500

    build enviroment

---
+++ build/precache-manifest.85818e46167ebda38dbf2ba75c8c31a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3d5446fc0a8ba30c3a2dbf6658c475",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af66bbca77d189ed931",
+    "url": "/static/css/main.e01feeb3.chunk.css"
+  },
+  {
+    "revision": "25374da14a783883e142",
+    "url": "/static/js/2.666b6620.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.666b6620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af66bbca77d189ed931",
+    "url": "/static/js/main.55bf44fa.chunk.js"
+  },
+  {
+    "revision": "1213a6a42a2c4b1e23dc",
+    "url": "/static/js/runtime-main.0367b6a8.js"
+  }
+]);
